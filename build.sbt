ef2a5d677783af82a6ae34ba990bb2975e15ac2d
Ivano Pagano <pagoda_5b@hotmail.com>
2016-05-06 03:10:43 +0200

    Add a client specification for tests

--- build.sbt
+++ build.sbt
@@ -8,7 +8,9 @@ resolvers += "spray repo" at "http://repo.spray.io"
 
 libraryDependencies ++= Seq(
   "org.scala-lang.modules" %% "scala-xml" % "1.0.4",
+  "org.scala-lang" % "scala-reflect" % "2.11.8",
   "io.spray" %% "spray-client" % "1.3.3",
-  "io.spray" %%  "spray-json" % "1.3.2",
+  "io.spray" %% "spray-json" % "1.3.2",
   "com.typesafe.akka" %% "akka-actor" % "2.4.1",
-  "com.typesafe.akka" %% "akka-actor" % "2.4.1")
+  "com.typesafe.akka" %% "akka-actor" % "2.4.1",
+  "org.scalatest" %% "scalatest" % "3.0.0-M15" % "test")
---
+++ src/test/scala/omdb/client/ClientSpec.scala
@@ -0,0 +1,42 @@
+package omdb.client
+
+import org.scalatest._
+
+/** Defines a contract for the omdb client
+  * Test common cases
+  */
+class ClientSpec extends FlatSpec with Matchers {
+
+  val client = new OpenMovieClientDef
+
+  "A OpenMovieClient" should "fetch a list of movie entries when called with a valid title" in {
+    val movies = client.listMoviesWithTitleLike("zoolander")
+
+    movies should not be empty
+  }
+
+  it should "return an empty list if the search title match no movie" in {
+    val movies = client.listMoviesWithTitleLike("zoorlander")
+
+    movies shouldBe empty
+  }
+
+  it should "convert the json results to a full OpenMovie object with the correct values" in {
+    val movie = client
+      .listMoviesWithTitleLike("zoolander")
+      .find(_.title == "Zoolander")
+
+    movie shouldBe defined
+
+    import  org.scalatest.OptionValues._
+
+    movie.value should have (
+      'title ("Zoolander"),
+      'year ("2001"),
+      'genre ("Comedy"),
+      'director ("Ben Stiller"),
+      'actors ("Ben Stiller, Owen Wilson, Christine Taylor, Will Ferrell")
+    )
+  }
+
+}
